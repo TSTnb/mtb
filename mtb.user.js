c60db1345cab72ec90da1c15a9fd855292087dda
morningpee <morningpee2@gmail.com>
2017-04-24 23:41:15 -0500

    userscript header

---
+++ mtb.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name Minimal Tetris Battle
+// @namespace minimaltetrisbattle
+// @description Reduces lag as much as possible
+// @include http*://tbc.tetrisfb.com/
+// @include http*://tbc.tetrisfb.com/index.php
+// @grant none
+// @run-at document-end
+// @version 0.0.1
+// @author morningpee
+// ==/UserScript==
