181b1aad2b12887b0fe18776682e337dd18b1c21
morningpee <morningpee2@gmail.com>
2017-04-25 19:30:10 -0500

    broader includes

--- mtb.user.js
+++ mtb.user.js
@@ -3,7 +3,8 @@
 // @namespace minimaltetrisbattle
 // @description Reduces lag as much as possible
 // @include http*://tbc.tetrisfb.com/
-// @include http*://tbc.tetrisfb.com/index.php
+// @include http*://tbc.tetrisfb.com/?*
+// @include http*://tbc.tetrisfb.com/index.php*
 // @grant none
 // @run-at document-end
 // @version 0.0.2
